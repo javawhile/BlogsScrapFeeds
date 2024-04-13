7002a89355e13b40bee44095744c59ae836bce91
Hardik Goel <hardikgoel19@gmail.com>
2024-04-13 10:59:46 +0530

    changes

--- src/main/java/com/blogs/scrap/MainApplication.java
+++ src/main/java/com/blogs/scrap/MainApplication.java
@@ -4,10 +4,12 @@ import com.blogs.scrap.feed.impl.BlogSite1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@PropertySource("classpath:/prod/application-prod.properties")
 public class MainApplication {
 
     public static void main(String[] args) {
